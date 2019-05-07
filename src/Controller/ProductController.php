b6795cebea5cf5936fb7c5e8cfd15740c6dafb02
Xavi Qu <xquerol19@gmail.com>
2019-05-07 18:59:45 +0200

    Change master and index templates

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,80 +2,24 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use App\Entity\Product;
 use Symfony\Component\HttpFoundation\Response;
 
-class ProductController extends Controller
+class ProductController extends AbstractController
 {
-    /**
-     * @Route("/product", name="product")
-     */
-    public function index()
-    {
-
-        // you can fetch the EntityManager via $this->getDoctrine()
-        // or you can add an argument to your action: index(EntityManagerInterface $entityManager)
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $product = new Product();
-        $product->setName('Mallot btt');
-        $product->setPrice(20.56);
-        $product->setShortDescription('Mallot per bicicleta de muntanya');
-
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($product);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
-
-
-        return $this->render('product/index.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
-    }
 
     /**
      * @Route("admin/product/", name="product_list")
      */
     public function show()
     {
-        /*$product = $this->getDoctrine()
-            ->getRepository(Product::class)
-            ->find($id);*/
-
-/*        if (!$product) {
-            throw $this->createNotFoundException(
-                'No product found for id '.$id
-            );
-        }*/
         $repository = $this->getDoctrine()->getRepository(Product::class);
 
-        // look for a single Product by its primary key (usually "id")
-        //$product = $repository->find($id);
-
-        // look for a single Product by name
-        //$product = $repository->findOneBy(['name' => 'Keyboard']);
-        // or find by name and price
-        //$product = $repository->findOneBy([
-        //    'name' => 'Keyboard',
-        //    'price' => 1999,
-        //]);
-
-        // look for multiple Product objects matching the name, ordered by price
-        //$products = $repository->findBy(
-        //    ['name' => 'Keyboard'],
-        //    ['price' => 'ASC']
-        //);
-
-        // look for *all* Product objects
         $products = $repository->findAll();
 
-        //return new Response('Check out this great product: '.$product->getName());
-
-        // render a template in the template, print things with {{ product.name }}
         return $this->render('admin/index.html.twig',
             ['product' => $products]
         );
@@ -129,8 +73,4 @@ class ProductController extends Controller
             'id' => $product->getId()
         ]);
     }
-
-
-
-
 }
